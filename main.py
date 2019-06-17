1354e400b9c75b1c95946a11cd2f3da636ab0cc0
cmtudor <cmtudor@crimson.ua.edu>
2019-06-17 14:31:32 -0500

    Finished?

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! python3
 import os, sys
+import directory
 import openpyxl
 from openpyxl import utils
 from openpyxl.utils import exceptions
@@ -11,53 +12,19 @@ def main():
     wb = get_workbook(name)
     a_sheet = wb.active
     # generating tuples for specific sections
-    gen_app_sec = tuple(a_sheet['A3':'C5'])
-    info_class = tuple(a_sheet['A7':'C9'])
-    sys_arch = tuple(a_sheet['A11':'C18'])
-    access_control = tuple(a_sheet['A20':'C30'])
-    data_trans_control = tuple(a_sheet['A32':'C36'])
-    db_control = tuple(a_sheet['A38':'C43'])
-    code_control = tuple(a_sheet['A45':'C47'])
-    confidentiality = tuple(a_sheet['A49':'C53'])
-    pw_control = tuple(a_sheet['A55':'C59'])
-    testing_control = tuple(a_sheet['A61':'C63'])
-    stride = tuple(a_sheet['A65':'C70'])
-    threat_analysis = tuple(a_sheet['A72':'C79'])
-    disaster_recov = tuple(a_sheet['A81':'C82'])
-    '''section_list = [
-                    gen_app_sec,
-                    info_class,
-                    sys_arch,
-                    access_control,
-                    data_trans_control,
-                    db_control,
-                    code_control,
-                    confidentiality,
-                    pw_control,
-                    testing_control,
-                    stride,
-                    threat_analysis,
-                    disaster_recov
-                    ]'''
-
-    '''section_names = ['General Application Security',
-                     'Information Classification',
-                     'System Architecture',
-                     'Access Control',
-                     'Data and Transaction Controls',
-                     'Database Controls',
-                     'Software and Proprietary and Code Control',
-                     'Confidentiality',
-                     'User Accounts and Password Control',
-                     'Testing Controls',
-                     'STRIDE Adherence',
-                     'Threat Analysis On Vulnerable Modules',
-                     'Disaster Recovery'
-                     ]'''
-
-    '''for i in range(0, len(section_list)):
-        print('*** ' + section_names[i] + ' ***')
-        input_data(section_list[i])'''
+    gen_app_sec = list(a_sheet['A3':'C5'])
+    info_class = list(a_sheet['A7':'C9'])
+    sys_arch = list(a_sheet['A11':'C18'])
+    access_control = list(a_sheet['A20':'C30'])
+    data_trans_control = list(a_sheet['A32':'C36'])
+    db_control = list(a_sheet['A38':'C43'])
+    code_control = list(a_sheet['A45':'C47'])
+    confidentiality = list(a_sheet['A49':'C53'])
+    pw_control = list(a_sheet['A55':'C59'])
+    testing_control = list(a_sheet['A61':'C63'])
+    stride = list(a_sheet['A65':'C70'])
+    threat_analysis = list(a_sheet['A72':'C79'])
+    disaster_recov = list(a_sheet['A81':'C82'])
     # dict of section tuples
     # sectionName : sectionTuple
     section_list = {
@@ -75,25 +42,56 @@ def main():
         'Threat Analysis On Vulnerable Modules': threat_analysis,
         'Disaster Recovery': disaster_recov
     }
+    section_rating = list()
+    # inputting data
     for k, v in section_list.items():
-        print('*** ' + k + ' ***')
+        print('*** ' + k + ' ***')  # section name
+        rating = input_data(v)  # section tuple
+        section_rating.append(rating)
+    # calc total rating
+    final_rating = calc_rating(section_rating)
+    a_sheet['C84'] = final_rating
+    # calc net score percentage
+    net_score = (final_rating / 315) * 100
+    a_sheet['C85'] = net_score
+    # saving edited workbook as a copy
+    print('Enter a new name for this document.')
+    copy_name = get_filename()
+    wb.save(copy_name)
+
+
+def calc_rating(section_rating):
+    total = 0
+    for rating in section_rating:
+        total += rating
+    return total
 
 
 def input_data(section):
+    total = 0
     for row in section:
         for cell in row:
-            if cell.value is not None:
-                print('    ' + cell.coordinate, cell.value)
+            if cell.coordinate[0] != 'C':
+                if cell.coordinate[0] == 'B':
+                    print('        Description: ' + str(cell.value))
+                else:
+                    if cell.value is not None:
+                        print('    ' + str(cell.value))
+                    else:
+                        print('    ' + cell.coordinate + ' None')
             else:
-                print('    ' + cell.coordinate + ' None')
+                print('    ' + 'Enter your rating (0-5):')
+                rating = input('      ' + '--')
+                cell.value = float(rating)
+                total = total + float(rating)
         print('----------------------------------------------------------------------')
-    print()
+    return total
 
 
 def change_cwd():
-    print(r"Changing CWD to C:\Users\chtudor\Documents\ArchitectureAssessments")
+    print('Changing CWD to ' + directory.directory)
     try:
-        os.chdir(r"C:\Users\chtudor\Documents\ArchitectureAssessments")
+        os.chdir(directory.directory)
         print("Directory changed.")
     except OSError:
         print("Error: unable to change CWD.")
